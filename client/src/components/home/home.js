f4a053c60968a9cc49d7c86bb250ed8c44e5711c
valentin-zhelyazkov <56276041+valentin-zhelyazkov@users.noreply.github.com>
2021-03-24 17:53:39 +0200

    Adding and fixing the position of the background image

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,4 +1,5 @@
 import './home.css';
+import img from '../../images/hotel.jpg';
 
 const home = () => {
     return (
@@ -6,19 +7,19 @@ const home = () => {
             <div className="added-hotels">
                 <a href="/details" className="added-hotel">
                     <img src="https://image.freepik.com/free-vector/flat-hotel-building_23-2148162501.jpg" alt=""
-                    className="picture-added-hotel"/>
+                        className="picture-added-hotel" />
                     <h3>Hilton Toronto</h3>
                     <span>Free rooms: 42</span>
                 </a>
                 <a href="" className="added-hotel">
                     <img src="https://image.freepik.com/free-vector/flat-hotel-building-town_52683-10040.jpg"
-                        alt="" className="picture-added-hotel"/>
+                        alt="" className="picture-added-hotel" />
                     <h3>Auckland Inn</h3>
                     <span>Free rooms: 35</span>
                 </a>
                 <a href="" className="added-hotel">
                     <img src="https://image.freepik.com/free-vector/showplace-illustration-with-all-famous-buildings-linear-banner_1416-984.jpg"
-                        alt="" className="picture-added-hotel"/>
+                        alt="" className="picture-added-hotel" />
                     <h3>Hotel Berlin</h3>
                     <span>Free rooms: 15</span>
                 </a>
